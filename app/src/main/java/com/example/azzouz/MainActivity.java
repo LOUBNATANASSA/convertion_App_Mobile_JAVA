a5cbf7ceb0c3df525728eac06143d38310b0369a
LOUBNATANASSA <loubnatanassa553@gmail.com>
2025-03-20 23:48:04 +0000

    commit changements

---
+++ app/src/main/java/com/example/azzouz/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.azzouz;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.txtnumber1), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        EditText txtnumber1 = findViewById(R.id.txtnumber1);
+        Button buttonconvert = findViewById(R.id.buttonconvert);
+        TextView txtresultat = findViewById(R.id.txtresultat);
+        Spinner spinner = findViewById(R.id.spinner);
+
+        buttonconvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txtnumber = txtnumber1.getText().toString();
+                if(txtnumber.isEmpty()){
+                    txtresultat.setText("00.00");
+                }else {
+                    int decimal = Integer.parseInt(txtnumber);
+                    String binary = Integer.toBinaryString(decimal);
+                    txtresultat.setText(binary);
+
+                }
+            }
+        });
+
+
+    }
+
+}
